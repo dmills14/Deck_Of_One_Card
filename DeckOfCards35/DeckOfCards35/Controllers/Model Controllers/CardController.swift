cec3efc5bad70c9e3e0f03fb97a7c3417ac27513
Drew Miller <drew-miller@lambdastudents.com>
2020-08-04 17:53:22 -0700

    got card to show up

---
+++ DeckOfCards35/DeckOfCards35/Controllers/Model Controllers/CardController.swift
@@ -0,0 +1,14 @@
+//
+//  CardController.swift
+//  DeckOfCards35
+//
+//  Created by Drew Miller on 8/4/20.
+//  Copyright © 2020 DevMtn. All rights reserved.
+//
+
+import Foundation
+
+class CardController {
+    
+    
+}
---
+++ DeckOfCards35/DeckOfCards35/Models/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  DeckOfCards35
+//
+//  Created by Drew Miller on 8/4/20.
+//  Copyright © 2020 DevMtn. All rights reserved.
+//
+
+import Foundation
